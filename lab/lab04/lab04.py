369817685f823fab55e6facef37d03dcd3dfddda
psosnin <philipsosnin@gmail.com>
2020-07-29 20:22:13 +0100

    first commit

---
+++ lab/lab04/lab04.py
@@ -0,0 +1,142 @@
+""" Lab 04 """
+
+
+this_file = __file__
+
+def skip_add(n):
+    """ Takes a number n and returns n + n-2 + n-4 + n-6 + ... + 0.
+
+    >>> skip_add(5)  # 5 + 3 + 1 + 0
+    9
+    >>> skip_add(10) # 10 + 8 + 6 + 4 + 2 + 0
+    30
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(this_file, 'skip_add',
+    ...       ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    return n + skip_add(n-2)
+
+def summation(n, term):
+
+    """Return the sum of the first n terms in the sequence defined by term.
+    Implement using recursion!
+
+    >>> summation(5, lambda x: x * x * x) # 1^3 + 2^3 + 3^3 + 4^3 + 5^3
+    225
+    >>> summation(9, lambda x: x + 1) # 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+    54
+    >>> summation(5, lambda x: 2**x) # 2^1 + 2^2 + 2^3 + 2^4 + 2^5
+    62
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(this_file, 'summation',
+    ...       ['While', 'For'])
+    True
+    """
+    assert n >= 1
+    "*** YOUR CODE HERE ***"
+    if n == 1: return term(n)
+    else: return term(n) + summation(n-1,term)
+
+def gcd(a, b):
+    """Returns the greatest common divisor of a and b.
+    Should be implemented using recursion.
+
+    >>> gcd(34, 19)
+    1
+    >>> gcd(39, 91)
+    13
+    >>> gcd(20, 30)
+    10
+    >>> gcd(40, 40)
+    40
+    """
+    "*** YOUR CODE HERE ***"
+    if a == b: return a
+    elif a > b: return b if a % b == 0 else gcd(b, a % b)
+    else: return a if b % a == 0 else gcd(a, b % a)
+
+def paths(m, n):
+    """Return the number of paths from one corner of an
+    M by N grid to the opposite corner.
+
+    >>> paths(2, 2)
+    2
+    >>> paths(5, 7)
+    210
+    >>> paths(117, 1)
+    1
+    >>> paths(1, 157)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 1 or m == 1: return 1
+    else: return paths(m - 1, n) + paths(m, n - 1)
+
+from math import log10, floor
+def max_subseq(n, l):
+    """
+    Return the maximum subsequence of length at most l that can be found in the given number n.
+    For example, for n = 20125 and l = 3, we have that the subsequences are
+        2
+        0
+        1
+        2
+        5
+        20
+        21
+        22
+        25
+        01
+        02
+        05
+        12
+        15
+        25
+        201
+        202
+        205
+        212
+        215
+        225
+        012
+        015
+        025
+        125
+    and of these, the maxumum number is 225, so our answer is 225.
+
+    >>> max_subseq(20125, 3)
+    225
+    >>> max_subseq(20125, 5)
+    20125
+    >>> max_subseq(20125, 6) # note that 20125 == 020125
+    20125
+    >>> max_subseq(12345, 3)
+    345
+    >>> max_subseq(12345, 0) # 0 is of length 0
+    0
+    >>> max_subseq(12345, 1)
+    5
+    """
+    "*** YOUR CODE HERE ***"
+    if l == 0: return 0
+    elif n < 10: return n
+    else: return max( (n % 10 + 10 * max_subseq(n // 10, l - 1)), max_subseq(n // 10, l))
+
+
+def max_sequence(n,l):
+    """Return the largest number that can be made up from any l digits of n
+    >>> max_sequence(20125, 3)
+    522
+    """
+    def sub_seq(n, l, count):
+        if floor(log10(n)) + 1 < count: return 0
+        elif l == 0: return 0
+        elif n < 10: return n
+        else: return max((n % 10 + 10*sub_seq(n//10, l - 1, 0)), sub_seq((n % 10)*10**floor(log10(n)) + n//10, l, count + 1)) 
+    return sub_seq(n,l,0)
---
+++ projects/hw03/hw03.py
@@ -0,0 +1,198 @@
+HW_SOURCE_FILE = 'hw03.py'
+#############
+# Questions #
+#############
+from math import floor, log
+def trace(fn):
+        def wrapped(*args):
+            print('-> ', fn, '(', *args, ')')
+            return fn(*args)
+        return wrapped
+    
+def num_sevens(x):
+    """Returns the number of times 7 appears as a digit of x.
+
+    >>> num_sevens(3)
+    0
+    >>> num_sevens(7)
+    1
+    >>> num_sevens(7777777)
+    7
+    >>> num_sevens(2637)
+    1
+    >>> num_sevens(76370)
+    2
+    >>> num_sevens(12345)
+    0
+    >>> from construct_check import check
+    >>> # ban all assignment statements
+    >>> check(HW_SOURCE_FILE, 'num_sevens',
+    ...       ['Assign', 'AugAssign'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if x == 0:
+        return 0
+    elif x % 10 == 7:
+        return num_sevens(x//10) + 1
+    else: return num_sevens(x//10)
+     
+def pingpong(n):
+   
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(7)
+    7
+    >>> pingpong(8)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    0
+    >>> pingpong(30)
+    6
+    >>> pingpong(68)
+    2
+    >>> pingpong(69)
+    1
+    >>> pingpong(70)
+    0
+    >>> pingpong(71)
+    1
+    >>> pingpong(72)
+    0
+    >>> pingpong(100)
+    2
+    >>> from construct_check import check
+    >>> # ban assignment statements
+    >>> check(HW_SOURCE_FILE, 'pingpong', ['Assign', 'AugAssign'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def count(k,c,direction):
+        if k == n: return c
+        elif direction: return switch(k+1,c+1,direction)
+        else: return switch(k+1,c-1,direction)
+    def switch(k,c,direction):
+        if k % 7 == 0 or num_sevens(k) > 0:
+            return count(k,c, not direction)
+        else: return count(k,c,direction)
+    return count(1,1,True)
+          
+def count_change(total):
+    """Return the number of ways to make change for total.
+
+    >>> count_change(7)
+    6
+    >>> count_change(10)
+    14
+    >>> count_change(20)
+    60
+    >>> count_change(100)
+    9828
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_change', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+    def countpartition(total, n):
+        if total <= 0: return 0
+        elif total == 1: return 1
+        elif n == 1: return 1
+        elif n == total: return 1 + countpartition(total,n/2)
+        else: return countpartition(total-n,n) + countpartition(total, n/2)
+        
+    return countpartition(total, 2**floor(log(total,2)))
+
+def missing_digits(n):
+    """Given a number a that is in sorted, increasing order,
+    return the number of missing digits in n. A missing digit is
+    a number between the first and last digit of a that is not in n.
+    >>> missing_digits(1248) # 3, 5, 6, 7
+    4
+    >>> missing_digits(1122) # No missing numbers
+    0
+    >>> missing_digits(123456) # No missing numbers
+    0
+    >>> missing_digits(3558) # 4, 6, 7
+    3
+    >>> missing_digits(4) # No missing numbers between 4 and 4
+    0
+    >>> from construct_check import check
+    >>> # ban while or for loops
+    >>> check(HW_SOURCE_FILE, 'missing_digits', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n < 10: return 0
+    if (n % 10) - (n //10)% 10 - 1 <= 0:
+        return missing_digits(n//10)
+    else: return (n % 10)- (n // 10) % 10 - 1 + missing_digits(n // 10)
+    
+
+
+###################
+# Extra Questions #
+###################
+
+def print_move(origin, destination):
+    """Print instructions to move a disk."""
+    print("Move the top disk from rod", origin, "to rod", destination)
+
+def move_stack(n, start, end):
+    """Print the moves required to move n disks on the start pole to the end
+    pole without violating the rules of Towers of Hanoi.
+
+    n -- number of disks
+    start -- a pole position, either 1, 2, or 3
+    end -- a pole position, either 1, 2, or 3
+
+    There are exactly three poles, and start and end must be different. Assume
+    that the start pole has at least n disks of increasing size, and the end
+    pole is either empty or has a top disk larger than the top n start disks.
+
+    >>> move_stack(1, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    >>> move_stack(2, 1, 3)
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 3
+    >>> move_stack(3, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 3 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 1
+    Move the top disk from rod 2 to rod 3
+    Move the top disk from rod 1 to rod 3
+    """
+    assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
+    "*** YOUR CODE HERE ***"
+    if n == 0: return
+    elif n == 1: 
+        print_move(start,end)
+    else:
+        spare = 6 - start - end
+        move_stack(n-1,start,spare)
+        move_stack(1,start,end)
+        move_stack(n-1,spare,end)
+    
+    
+
+from operator import sub, mul
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    >>> from construct_check import check
+    >>> # ban any assignments or recursion
+    >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
+    True
+    """
+    return (lambda f: lambda k: f(f,k))(lambda l,j: j if j == 1 else mul(j, l(l,sub(j,1))))
